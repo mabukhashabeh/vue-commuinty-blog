955a498ffb29a9eb0adb9732e1d920afc6e3d779
mohammadabukhashabeh <mohammad.abukhashabeh97@gmail.com>
2020-12-19 16:17:13 +0200

    Add bootstrap and jquery

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
+    <div>
 
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
+    import 'bootstrap/dist/css/bootstrap.min.css'
+    import 'jquery/src/jquery.js'
+    import 'bootstrap/dist/js/bootstrap.min.js'
 
-}
+    export default {
+        name: 'App',
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
