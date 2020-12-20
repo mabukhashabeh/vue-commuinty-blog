7f4bc8ee8eb4cc718d69d3d0fbcc8cf5ab16082e
mohammadabukhashabeh <mohammad.abukhashabeh97@gmail.com>
2020-12-20 11:51:43 +0200

    Using view router with components for SPA

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <navbar></navbar>
+        <router-view></router-view>
     </div>
 </template>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Login Component</h1>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
